031bc5ad4a96b73c6509152dec12aa45cf465578
Kara-Nemet <kn485120@ohio.edu>
2022-01-19 17:18:31 -0500

    trying to get a program to github for the first time lollll

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: KaraBeth Nemet
+ *        Date: 01-19-2022
+ * Description: Practice using GitHub using this program :)
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
